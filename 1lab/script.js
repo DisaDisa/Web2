2c6bb4d3224ace5d7e3086796cec870e8b72f399
DisaDisa <denisarhipov97@gmail.com>
2019-10-09 10:09:08 +0300

    split js and html. Removed error allert.

--- 1lab/script.js
+++ 1lab/script.js
@@ -1,31 +1,54 @@
-button.onclick = function() {
+document.getElementById("form1")
+        .addEventListener("submit", function (event) {
+        event.preventDefault()
+        printWeather()
+});
+
+function printWeather() {
+    var source = document.getElementById("text-template").innerHTML
+    var template = Handlebars.compile(source)
+
+    var data = getWeather()
+    if (isRealValue(data)) {
+        var result = template(data)
+        document.getElementById('content').innerHTML = result
+    }
+}
+
+function printError(req) {
+    var source = document.getElementById("error-template").innerHTML
+    var template = Handlebars.compile(source)
+
+    var data = {
+        status: req.status,
+        errorText: req.statusText
+    }
+    var result = template(data)
+    document.getElementById('content').innerHTML = result   
+}
+
+function isRealValue(obj) {
+    return obj && obj !== 'null' && obj !== 'undefined';
+}
+
+function getWeather(){
     var enteredCity = document.getElementById('userCity').value
     var req = new XMLHttpRequest()
     req.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + enteredCity + '&APPID=839cba68bbf9459f78ec72d97881a87f', false)
     req.send()
     if (req.status !== 200) {
-        var errorMsg = "Error:" + req.status + ' ' + req.statusText
-        alert(errorMsg)  
+        printError(req)
+        //var errorMsg = "Error:" + req.status + ' ' + req.statusText
+        //alert(errorMsg)  
     } else {
         var response = JSON.parse(req.responseText);
         var data = {
             city: response.name,
             temp: (response.main.temp - 273.15).toFixed(2),
-            wind_speed: response.wind.speed,
+            windSpeed: response.wind.speed,
             sky: response.weather[0].description,
         }
 
-        
-
-        var source = document.getElementById("entry-template").innerHTML;
-		var template = Handlebars.compile(source);
-
-		var data = {city_name: data.city, temp: data.temp, wind_speed: data.wind_speed, sky: data.sky};
-		var result = template(data);
-		printMessage(result)
+        return data;
     }
-}
-
-function printMessage(text) {
-	document.getElementById('content').innerHTML = text
 }
